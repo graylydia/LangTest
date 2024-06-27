6670b8ff9989a075bab9b338b23e2619d7fa16b0
graylydia <103657822+graylydia@users.noreply.github.com>
2024-06-27 17:51:15 -0500

    Add files via upload

---
+++ langtest_qa2.py
@@ -0,0 +1,47 @@
+import os
+from langtest import Harness
+from huggingface_hub import login
+
+# Log in to Hugging Face Hub
+login(token="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")
+
+# Set environment for HF hub
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+
+# Configure Harness
+harness = Harness(
+    task="question-answering",
+    model={"model": "mistralai/Mixtral-8x7B-v0.1", "hub": "huggingface"},
+    data={"data_source": "Open-Orca/OpenOrca", "source": "huggingface", "split": "train"}
+)
+
+harness.configure({
+    'model_parameters': {
+        'temperature': 0,
+        'max_tokens': 64
+    },
+    'tests': {
+        'defaults': {
+            'min_pass_rate': 1.00
+        },
+        'robustness': {
+            'lowercase': {'min_pass_rate': 0.70}
+            # 'uppercase': {'min_pass_rate': 0.70}
+        }
+    }
+})
+
+# Generate test cases
+harness.generate()
+
+# Display test cases
+harness.testcases()
+
+# Run the tests outlined in harness configuration
+harness.run()
+
+# Generate results
+harness.generated_results
+
+# Summary of results
+harness.report()
