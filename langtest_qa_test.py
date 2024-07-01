395a38e771641f8830426fd6a43b4268ae2dab81
graylydia <103657822+graylydia@users.noreply.github.com>
2024-07-01 09:23:28 -0500

    Update langtest_qa_test.py

--- langtest_qa_test.py
+++ langtest_qa_test.py
@@ -8,28 +8,38 @@ login(token="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")
 # Set environment for HF hub
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
-# Configure Harness
-harness = Harness(
-    task="question-answering",
-    model={"model": "meta-llama/Meta-Llama-3-8B", "hub": "huggingface"},
-    data={"data_source": "Open-Orca/OpenOrca", "source": "huggingface", "split": "train"}
-)
+# Define the model and data source
+model={"model": "meta-llama/Meta-Llama-3-8B","hub":"huggingface"}
+data={"data_source": "Open-Orca/OpenOrca", "source": "huggingface", "split": "train"}
 
-harness.configure({
-    'model_parameters': {
-        'temperature': 0,
-        'max_tokens': 64
-    },
-    'tests': {
-        'defaults': {
-            'min_pass_rate': 1.00
-        },
-        'robustness': {
-            'lowercase': {'min_pass_rate': 0.70}
-            # 'uppercase': {'min_pass_rate': 0.70}
-        }
+# Create a Harness object
+harness = Harness(task="question-answering", model=model, data=data)
+
+# Congifure the Harness
+# harness.configure({
+#     'model_parameters': {
+#         'temperature': 0,
+#         'max_tokens': 64
+#     },
+#     'tests': {
+#         'defaults': {
+#             'min_pass_rate': 1.00
+#         },
+#         'robustness': {
+#             'lowercase': {'min_pass_rate': 0.70}
+#             # 'uppercase': {'min_pass_rate': 0.70}
+#         }
+#     }
+# })
+harness.configure(
+    {
+        "evaluation": {"metric":"embedding_distance","distance":"cosine","threshold":0.9},
+        "embeddings":{"model":"sentence-transformers/all-mpnet-base-v2","hub":"huggingface"},
+        'tests':{'defaults': {'min_pass_rate':0.66},
+                 'dyslexia_word_swap':{'min_pass_rate':0.60}
+                }
     }
-})
+)
 
 # Generate test cases
 harness.generate()
