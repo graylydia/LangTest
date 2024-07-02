b82a202bcd7c7e87c1fd6a419ad83c4c5315b46a
graylydia <103657822+graylydia@users.noreply.github.com>
2024-07-02 09:15:11 -0500

    Update langtest_qa.py

--- langtest_qa.py
+++ langtest_qa.py
@@ -3,33 +3,55 @@ import os
 from langtest import Harness
 from huggingface_hub import login
 
-login(token="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")
+login(token="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")
 
 # Set environment for HF hub
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
 # Configure Harness
+# harness = Harness(
+#                   task="question-answering",
+#                   model={"model": "mistralai/Mixtral-8x7B-v0.1","hub": "huggingface-inference-api"},
+#                   data={"data_source" :"Open-Orca/OpenOrca", "source": "huggingface", "split":"train"
+#                        }
+# )
+
+# harness.configure({
+#     'model_parameters': {
+#         'temperature': 0,
+#         'max_tokens': 64
+#     },
+
+#     'tests': {
+#       'defaults':{
+#         'min_pass_rate': 1.00
+#       },
+
+#       'robustness':{
+#         'lowercase': {'min_pass_rate': 0.70}
+#         # 'uppercase': {'min_pass_rate': 0.70}
+#       }
+#     }
+# })
 harness = Harness(
-                  task="question-answering",
-                  model={"model": "mistralai/Mixtral-8x7B-v0.1","hub": "huggingface-inference-api"},
-                  data={"data_source" :"Open-Orca/OpenOrca", "source": "huggingface", "split":"train"
-                       }
-)
+                  task="question-answering", 
+                  model={"model": "google/flan-t5-small","hub":"huggingface-inference-api"}, 
+                  data={"data_source" :"NQ-open",
+                        "split":"test-tiny"}
+                  )
 
 harness.configure({
     'model_parameters': {
-        'temperature': 0,
-        'max_tokens': 64
+      'max_tokens': 64
     },
-
     'tests': {
       'defaults':{
         'min_pass_rate': 1.00
       },
 
       'robustness':{
-        'lowercase': {'min_pass_rate': 0.70}
-        # 'uppercase': {'min_pass_rate': 0.70}
+        'lowercase': {'min_pass_rate': 0.70},
+        'uppercase': {'min_pass_rate': 0.70}
       }
     }
 })
