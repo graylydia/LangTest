0619f4058b38edc98baebed4c90875856de72e5e
graylydia <103657822+graylydia@users.noreply.github.com>
2024-07-11 07:56:41 -0500

    Update qa_demo.py

--- qa_demo.py
+++ qa_demo.py
@@ -1,70 +1,74 @@
+# #Import Harness from the LangTest library
+# from langtest import Harness
+
+# import os
+# os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+
+# harness = Harness(
+#                   task="question-answering", 
+#                   model={"model": "google/flan-t5-small","hub": "huggingface-inference-api"},
+#                   data={"data_source" :"BoolQ",
+#                         "split":"test-tiny"}
+#                   )
+
+# harness.configure({
+#     'model_parameters': {
+#         'temperature': 0,
+#         'max_tokens': 64
+#     },
+    
+#     'tests': {
+#       'defaults':{
+#         'min_pass_rate': 1.00
+#       },
+
+#       'robustness':{
+#         'lowercase': {'min_pass_rate': 0.70},
+#         'uppercase': {'min_pass_rate': 0.70}
+#       }
+#     }
+# })
+
+# harness.generate()
+
+# harness.testcases()
+
+# harness.run()
+
+# harness.generated_results()
+
+# harness.report()
+
+# Import necessary libraries
+import os
+
+# Set your OpenAI API key
+os.environ["OPENAI_API_KEY"] = ""
+
 #Import Harness from the LangTest library
 from langtest import Harness
 
-# Setup and configure harness
-# harness = Harness(
-#     task="summarization",
-#     model={"model": "facebook/opt-1.3b", "hub":"huggingface"},
-#     data={"data_source" :"XSum",
-#           "split":"test-tiny"},
-#     config={
-#       'model_parameters': {
-#         'max_tokens': 32
-#       },
+# Define the model and data source
+model={"model": "text-davinci-003","hub":"openai"}
+data={"data_source" :"CommonsenseQA-test-tiny"}
 
-#       'tests': {
-#         'defaults':{
-#           'min_pass_rate': 1.00
-#         },
-
-#         'robustness':{
-#           'lowercase': {'min_pass_rate': 0.70},
-#           'add_typo': {'min_pass_rate': 0.70}
-#         }
-#       }
-#     })
-
-harness = Harness(
-    task="question-answering",
-    model={"model": "facebook/opt-1.3b", "hub":"huggingface"},
-    data={"data_source" :"BoolQ",
-          "split":"test-tiny"},
-    config={
-      'model_parameters': {
-        'max_tokens': 32
-      },
-
-      'tests': {
-        'defaults':{
-          'min_pass_rate': 1.00
-        },
-
-        'robustness':{
-          'lowercase': {'min_pass_rate': 0.70},
-          'add_typo': {'min_pass_rate': 0.70}
-        }
-      }
-    })
+# Create a Harness object
+harness = Harness(task="question-answering", model=model, data=data)
 
 harness.configure(
 {
- 'tests': {
-    'defaults': {'min_pass_rate': 0.65},
-      'robustness': {
-        'lowercase': {'min_pass_rate': 0.66, 'prob': 0.50},
-        'uppercase':{'min_pass_rate': 0.60, 'prob': 0.70},
-      }
-  }
-})
+ "evaluation": {"metric":"embedding_distance","distance":"cosine","threshold":0.9},
+ "embeddings":{"model":"text-embedding-ada-002","hub":"openai"},
+ # Note: To switch to the Hugging Face model, change the "hub" to "huggingface" and set the "model" to the desired Hugging Face embedding model.
+ 
+'tests': {'defaults': {'min_pass_rate': 0.65},
 
-#harness.data = harness.data[:10]
+           'robustness': {'add_ocr_typo': {'min_pass_rate': 0.66},
+                          'dyslexia_word_swap':{'min_pass_rate': 0.60}
+                         }
+          }
+ }
 
-harness.generate()
+)
 
-harness.testcases()
-
-harness.run()
-
-harness.generated_results()
-
-harness.report()
+harness.generate().run().generated_results()
