2d354f5856fcff686c58c0cff36ca6adaed394ab
graylydia <103657822+graylydia@users.noreply.github.com>
2024-07-11 07:55:58 -0500

    Update new_demo.py

--- new_demo.py
+++ new_demo.py
@@ -2,10 +2,32 @@
 from langtest import Harness
 
 # Setup and configure harness
+# harness = Harness(
+#     task="summarization",
+#     model={"model": "facebook/opt-1.3b", "hub":"huggingface"},
+#     data={"data_source" :"XSum",
+#           "split":"test-tiny"},
+#     config={
+#       'model_parameters': {
+#         'max_tokens': 32
+#       },
+
+#       'tests': {
+#         'defaults':{
+#           'min_pass_rate': 1.00
+#         },
+
+#         'robustness':{
+#           'lowercase': {'min_pass_rate': 0.70},
+#           'add_typo': {'min_pass_rate': 0.70}
+#         }
+#       }
+#     })
+
 harness = Harness(
-    task="summarization",
+    task="question-answering",
     model={"model": "facebook/opt-1.3b", "hub":"huggingface"},
-    data={"data_source" :"XSum",
+    data={"data_source" :"BoolQ",
           "split":"test-tiny"},
     config={
       'model_parameters': {
@@ -23,6 +45,17 @@ harness = Harness(
         }
       }
     })
+
+harness.configure(
+{
+ 'tests': {
+    'defaults': {'min_pass_rate': 0.65},
+      'robustness': {
+        'lowercase': {'min_pass_rate': 0.66, 'prob': 0.50},
+        'uppercase':{'min_pass_rate': 0.60, 'prob': 0.70},
+      }
+  }
+})
 
 #harness.data = harness.data[:10]
 
